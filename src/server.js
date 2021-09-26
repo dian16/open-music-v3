9f427d296073308d276376a38d13e83ddfb1ad6e
dian16 <dianyunadi19@gmail.com>
2021-09-26 15:57:00 +0700

    authentication implemented

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
+
 const Hapi = require('@hapi/hapi')
+const Jwt = require('@hapi/jwt')
 
 // songs
 const MusicService = require('./services/postgres/MusicService')
@@ -11,9 +13,16 @@ const users = require('./api/users')
 const UsersService = require('./services/postgres/UsersService')
 const UsersValidator = require('./validator/users/')
 
+// authentications
+const authentications = require('./api/authentications')
+const AuthenticationsService = require('./services/postgres/AuthenticationsService')
+const TokenManager = require('./tokenize/TokenManager')
+const AuthenticationsValidator = require('./validator/authentications')
+
 const init = async () => {
   const musicService = new MusicService()
   const usersService = new UsersService()
+  const authenticationsService = new AuthenticationsService()
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -23,6 +32,30 @@ const init = async () => {
         origin: ['*']
       }
     }
+  })
+
+  // regist plugin eksternal
+  await server.register([
+    {
+      plugin: Jwt
+    }
+  ])
+
+  // mendefinisikan strategy auth jwt
+  server.auth.strategy('musicsapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id
+      }
+    })
   })
 
   await server.register([
@@ -38,6 +71,15 @@ const init = async () => {
       options: {
         service: usersService,
         validator: UsersValidator
+      }
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator
       }
     }
   ])
